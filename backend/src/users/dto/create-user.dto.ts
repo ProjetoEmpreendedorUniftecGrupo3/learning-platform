fbe05615d1c9e92b2216cd86a13d6062d29a7455
Eduardo Faggion <edufaggion@gmail.com>
2025-03-22 18:44:50 -0300

    adiciona validação de senha no backend

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,5 +1,5 @@
 import { Type } from "class-transformer";
-import { IsDate, IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+import { IsDate, IsEmail, IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class CreateUserDto {
 	@IsString()
@@ -16,6 +16,18 @@ export class CreateUserDto {
 	@IsNotEmpty()
 	@MinLength(8)
 	@MaxLength(32)
+	@Matches(/(?=.*\d)/, {
+		message: "A senha deve conter pelo menos um número",
+	})
+	@Matches(/(?=.*[a-z])/, {
+		message: "A senha deve conter pelo menos uma letra minúscula",
+	})
+	@Matches(/(?=.*[A-Z])/, {
+		message: "A senha deve conter pelo menos uma letra maiúscula",
+	})
+	@Matches(/(?=.*[\W_])/, {
+		message: "A senha deve conter pelo menos um caractere especial",
+	})
 	password: string;
 
 	@IsDate()
