a7291399620247e54600821a829be023bc89b083
Eduardo Faggion <edufaggion@gmail.com>
2025-04-26 18:22:47 -0300

    fix: bloqueio de desafios

--- frontend/src/pages/auth/Trail/index.tsx
+++ frontend/src/pages/auth/Trail/index.tsx
@@ -275,26 +275,26 @@ const CategoryNode = ({
 						}}
 					>
 						{challenge.completed ? <LockOpen color="green" /> : <Lock />}
+						{blocked && (
+							<Box
+								position="absolute"
+								top="0"
+								left="0"
+								w="100%"
+								h="100%"
+								bg="rgba(0,0,0,0.5)"
+								borderRadius="full"
+								display="flex"
+								alignItems="center"
+								justifyContent="center"
+							>
+								<Lock color="white" />
+							</Box>
+						)}
 					</Flex>
 					<Text textAlign="center" color={challenge.completed ? "green.700" : "black"}>
 						Desafio
 					</Text>
-					{blocked && (
-						<Box
-							position="absolute"
-							top="0"
-							left="0"
-							w="100%"
-							h="100%"
-							bg="rgba(0,0,0,0.5)"
-							borderRadius="full"
-							display="flex"
-							alignItems="center"
-							justifyContent="center"
-						>
-							<Lock color="white" />
-						</Box>
-					)}
 				</Flex>
 			)}
 		</Flex>
