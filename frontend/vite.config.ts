f450ab765ecec3205fee3df3063ee898da88fcd6
Eduardo Faggion <edufaggion@gmail.com>
2025-05-20 22:20:32 -0300

    atualiza allowedHosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
 	preview: {
-		allowedHosts: ["learning-frontend-a3e534e7a3d4.herokuapp.com"],
+		allowedHosts: ["learning-platform-frontend.onrender.com"],
 	},
 });
